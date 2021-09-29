188ce0eb39e533ee248e83a2f085b33c08e197f6
Diego Romero <diegoromeropersonal@gmail.com>
2021-09-28 22:38:15 -0300

    AIOSUZNDFBIHJSADFBAPSFBVEHWQN0JOKP´2+.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import FacebookLogin from "react-facebook-login";
 
 export default () => {
-  return <h1>STANDARD REACT SETUP</h1>;
+  const [messages, setMessages] = useState([]);
+  const [notifications, setNotifications] = useState([]);
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    socket.on("connection", (socketData) => {
+      socketData.join("USERIDTOBEADDED");
+    });
+    socket.on("message", (messageObj) => {
+      setMessages((prevState) => [messageObj, ...prevState]);
+    });
+    socket.on("notification", (notiObj) => {
+      setNotifications((prevState) => [notiObj, ...prevState]);
+    });
+    return () => socket.disconnect();
+  });
+  const handleClick = (res) => {
+    console.log(res);
+  };
+  return (
+    <div>
+      {!!notifications.length && (
+        <ul>
+          <li>
+            <h4>NOTIFICATIONS</h4>
+          </li>
+          {notifications.map((noti) => (
+            <li key={noti}>{noti}</li>
+          ))}
+        </ul>
+      )}
+      {!!messages.length && (
+        <ul>
+          <li>
+            <h2>MESSAGES</h2>
+          </li>
+          {messages.map((msg) => (
+            <li key={msg}>{msg}</li>
+          ))}
+        </ul>
+      )}
+      <FacebookLogin
+        appId="1474724352891583"
+        autoLoad
+        fields="first_name,last_name,birthday,email,picture"
+        callback={handleClick}
+      />
+    </div>
+  );
 };
