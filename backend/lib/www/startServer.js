2c1e31c17e2a6af60bc212924e502da0fa1b3e00
Diego Romero <diegoromeropersonal@gmail.com>
2021-10-05 06:46:58 -0300

    Fix deploy for heroku PORT

--- backend/lib/www/startServer.js
+++ backend/lib/www/startServer.js
@@ -1,13 +1,13 @@
 const { Server } = require("socket.io");
 const app = require("../../app");
 
-const server = app.listen(3000, () => console.log("3000"));
+const server = app.listen(process.env.PORT || 3000, () => console.log("3000"));
 
 /* REAL-TIME SERVER CONFIG */
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: "https://diegoromerodev.github.io/",
   },
 });
 
